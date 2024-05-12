6f22dfa6964c776b85f2420c5de50722179828e5
Jitendra Kumar Singh <132134089+jitutestgithub@users.noreply.github.com>
2024-05-12 21:56:30 +0530

    Add files via upload

---
+++ build/classes/attmm/BalanceEquiry.rs
@@ -0,0 +1,1 @@
+attmm.BalanceEnquiry
---
+++ src/attmm/Conn.java
@@ -0,0 +1,21 @@
+package attmm;
+
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","jitendrakumarsingh123");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
